d236a348961c2c5ff3484538ee9d482deda036a0
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-10-05 17:15:38 +0200

    details

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -16,19 +16,32 @@ const Projets = () => {
                   <h3>CANDILOC</h3>
                   <h4>Louez votre candidat politique préféré.</h4>
                 </div>
-                <p>
-                  {/* <u>OBJECTIF</u> */}
-                  <img src="./assets/images/objectif.png" className="picto" />
-                  création d'un site de location type "airbnb" avec un tout
-                  autre thème.
-                  <br />
-                  {/* <u>TEMPS</u> */}
-                  <img src="./assets/images/temps.png" className="picto" />1 semaine
-                  <br />
+                <div className="card-details">
+                  <div>
+                    <img
+                      src="./assets/images/objectif.png"
+                      className="picto"
+                      alt="objectif"
+                    />
+                    <p>
+                      création d'un site de location type "airbnb" avec un tout
+                      autre thème.
+                    </p>
+                  </div>
+                  <div>
+                    <img
+                      src="./assets/images/temps.png"
+                      className="picto"
+                      alt="temps"
+                    />
+                    <p>1 semaine</p>
+                  </div>
+                </div>
+                <p className="card-description">
                   Application web réalisée en équipe lors de ma formation au
                   Wagon.
-                  <br />
                 </p>
+                <div className="card-logos"></div>
               </div>
             </div>
           </a>
@@ -36,24 +49,34 @@ const Projets = () => {
         <div className="pets">
           <a href="https://www.placeforpets.xyz/" target="_blank">
             <div className="card">
-              <img src="./assets/images/PLACE_FOR_PETS_v2.png" className="visuel" />
+              <img
+                src="./assets/images/PLACE_FOR_PETS_v2.png"
+                className="visuel"
+              />
               <div className="card-content">
                 <div className="card-title">
                   <h3>PLACE FOR PETS</h3>
                   <h4>Vous partez en vacances ? Eux aussi.</h4>
                 </div>
-                <p>
-                  <img src="./assets/images/objectif.png" className="picto" />
-                  Réalisation d'une application qui calcule en temps réel le
-                  nombre de place restante dans des pensions à une courte
-                  distance d'un trajet en fonction du type d'animal.
-                  <br />
-                  <img src="./assets/images/temps.png" className="picto" />3 semaines
-                  <br />
+                <div className="card-details">
+                  <div>
+                    <img src="./assets/images/objectif.png" className="picto" />
+                    <p>
+                      Réalisation d'une application qui calcule en temps réel le
+                      nombre de place restante dans des pensions à une courte
+                      distance d'un trajet en fonction du type d'animal.
+                    </p>
+                  </div>
+                  <div>
+                    <img src="./assets/images/temps.png" className="picto" />
+                    <p>3 semaines</p>
+                  </div>
+                </div>
+                <p className="card-description">
                   Application web format mobile (390x844) réalisée en équipe
                   lors de ma formation au Wagon.
-                  <br />
                 </p>
+                <div className="card-logos"></div>
               </div>
             </div>
           </a>
@@ -67,15 +90,18 @@ const Projets = () => {
                   <h3>PORTFOLIO</h3>
                   <h4>Création d'un portfolio 'from scratch'.</h4>
                 </div>
-                <p>
+                <div className="card-details">
                   <img src="./assets/images/objectif.png" className="picto" />
-                  Réalisation d'un site vitrine avec REACT.
-                  <br />
-                  <img src="./assets/images/temps.png" className="picto" />1 semaines
-                  <br />
+                  <p>Réalisation d'un site vitrine avec REACT.</p>
+                  <div>
+                    <img src="./assets/images/temps.png" className="picto" />
+                    <p>1 semaines</p>
+                  </div>
+                </div>
+                <p className="card-description">
                   Application web réalisée en solo après ma formation au Wagon.
-                  <br />
                 </p>
+                <div className="card-logos"></div>
               </div>
             </div>
           </a>
